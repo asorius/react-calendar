f8eeab3fa0e6ced34b444b5f0dc514d61ef9766a
asorius <18assdd@gmail.com>
2022-07-17 18:46:29 +0100

    updated data generation, call to action button, svg arrow

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,15 @@
 import './styling/main-header.css';
+import './styling/action-button.css';
 const MainHeader = () => {
   return (
     <div className='header'>
       <h1>Dentistry</h1>
       <h2>Staffordshire</h2>
+      <a href='#about'>
+        <button type='button' className='fill'>
+          Find us
+        </button>
+      </a>
     </div>
   );
 };
--- src/components/utils/index.ts
+++ src/components/utils/index.ts
@@ -32,17 +32,38 @@ const generateData = () => {
   const date = new Date();
   const month = date.getMonth() + 1;
   const days = new Date(date.getFullYear(), month, 0).getDate();
-  const rng = () => Math.floor(Math.random() * days) + 1;
-  const randomNumbers = [...Array(15)].map((el) => rng());
+  const times = Array.from(Array(8), (e, i) => i + 8 + ':00');
+  const rng = (range: number) => Math.floor(Math.random() * range) + 1;
+  const mockBookRate = () => +(1 / rng(times.length)).toFixed(2);
+  const randomNumbers = [...Array(15)].map((el) => rng(days));
   const removedDuplicates = Array.from(new Set(randomNumbers));
-  const list = removedDuplicates.map((el) => ({
-    day: el,
-    month: month,
-    year: 2022,
-    bookRate: +Math.random().toFixed(2),
-    timeAvailability: ['09:00', '10:00', '11:00', '12:00'],
-  }));
+  const list = removedDuplicates.map((el, i) => {
+    const bookRate = mockBookRate();
+    return {
+      day: el,
+      month: month,
+      year: 2022,
+      bookRate,
+      timeAvailability: times.slice(0, times.length - times.length * bookRate),
+    };
+  });
 
-  return list;
+  const allDaysArray = [...Array(days)].map((val, ind) => ++ind);
+  const replaced = allDaysArray.map((el) => {
+    const cellWithData = list.find((e) => e.day === el);
+
+    if (cellWithData) {
+      return cellWithData;
+    } else {
+      return {
+        day: el,
+        month,
+        year: 2022,
+        bookRate: 0,
+        timeAvailability: times,
+      };
+    }
+  });
+  return replaced;
 };
 export default generateData;
